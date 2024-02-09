281848655974f537498954e4e12d618885b1a345
Armlockx <reus_julio@hotmail.com>
2024-02-08 22:33:41 -0300

    change index route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('./routes/index.js')
+    res.sendFile('./routes/index.js');
 });
 //app.use("/home", home);
 
